3fdbcb0c49b511e5432f7e218923903852dcf4fd
Safiy Cham <csafi13@yahoo.com>
2017-09-15 08:53:44 -0700

    add passing spec test for saving values into a class variable of class Word.

--- lib/word.rb
+++ lib/word.rb
@@ -1,7 +1,16 @@
 class Word
+  @@words = []
 
   attr_accessor :word
   def initialize (attributes)
     @word = attributes.fetch(:word)
+  end
+
+  def save
+    @@words.push(self)
+  end
+
+  def self.all
+    @@words
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,10 +4,19 @@ require 'pry'
 
 describe("#Word") do
   describe("#word") do
-    it("checks for the value of a Word instance within the attributes hash") do
+    it("checks for the value of the Word instance within the attributes hash") do
       word1 = Word.new({:word=> "Alibi"})
       expect(word1.word()).to(eq("Alibi"))
     end
   end
 
+  describe("#save") do
+    it("saves Word instances to a list in the class variable called @@words") do
+      word1 = Word.new({:word=> "Alibi"})
+      word2 = Word.new({:word=> "Bully"})
+      word1.save
+      word2.save
+      expect(Word.all).to(eq([word1, word2]))
+    end
+  end
 end
