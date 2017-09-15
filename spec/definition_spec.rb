c1047237553cd4605eeea11ce3cefe258005530b
Safiy Cham <csafi13@yahoo.com>
2017-09-15 16:08:54 -0700

    add passing spec to save word definition.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -10,4 +10,17 @@ describe("#Definition") do
       expect(word1.definition.define).to(eq("The solution to a problem"))
     end
   end
+
+  describe("#save") do
+    it("checks for the value of the instance 'define' of the class 'Definition', which is an instance 'definition' of class 'Word'") do
+      word1 = Word.new({:word=> "Answer"})
+      word1.definition.define = "The solution to a problem"
+      word1.save
+      word2 = Word.new({:word=> "Brake"})
+      word2.definition.define = "To stop"
+      word2.save
+      expect(word1.definition.define).to(eq("The solution to a problem"))
+      expect(word2.definition.define).to(eq("To stop"))
+    end
+  end
 end
