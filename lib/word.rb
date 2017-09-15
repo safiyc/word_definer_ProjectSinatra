b93b82c22a05eefa5870da1aa80ae1a411cdc100
Safiy Cham <csafi13@yahoo.com>
2017-09-15 12:37:06 -0700

    add passing spec test to increment the id by 1 of a new word.

--- lib/word.rb
+++ lib/word.rb
@@ -1,9 +1,11 @@
 class Word
   @@words = []
+  attr_reader :id
+  attr_accessor :word
 
-  attr_reader :word
   def initialize (attributes)
     @word = attributes.fetch(:word)
+    @id = @@words.length + 1
   end
 
   def save
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -14,8 +14,8 @@ describe("#Word") do
     it("saves Word instances to a list in the class variable called @@words") do
       Word.clear
       word1 = Word.new({:word=> "Alibi"})
-      word2 = Word.new({:word=> "Busted"})
       word1.save
+      word2 = Word.new({:word=> "Bunny"})
       word2.save
       expect(Word.all).to(eq([word1, word2]))
     end
@@ -24,13 +24,24 @@ describe("#Word") do
   describe(".clear") do
     it("clears the list inside @@contacts") do
       word1 = Word.new({:word=> "Alibi"})
-      word2 = Word.new({:word=> "Busted"})
-      word3 = Word.new({:word=> "Criminal"})
       word1.save
+      word2 = Word.new({:word=> "Alien"})
       word2.save
+      word3 = Word.new({:word=> "Crazy"})
       word3.save
       Word.clear
       expect(Word.all).to(eq([]))
     end
   end
+
+  describe("#id") do
+  it("increments an id by 1 each time a new item is added") do
+    word1 = Word.new({:word=> "Alien"})
+    word1.save
+    word2 = Word.new({:word=> "Bundle"})
+    word2.save
+    expect(word1.id()).to(eq(1))
+    expect(word2.id()).to(eq(2))
+  end
+end
 end
