fa03ec928aaaab3528f5a819cd5862629c15b6ad
Safiy Cham <csafi13@yahoo.com>
2017-09-15 20:58:31 -0700

    add passing unit test for saving words and definitions.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -16,4 +16,14 @@ describe("#Word") do
       expect(word1.word_definition()).to(eq("The solution to a problem"))
     end
   end
+
+  describe("#save") do
+    it("saves words and definitons") do
+      word1 = Word.new({:word=> "Answer", :word_definition=> "The solution to a problem"})
+      word1.save
+      word2 = Word.new({:word=> "Tired", :word_definition=> "The result of spending a whole day figuring out coding errors"})
+      word2.save
+      expect(Word.all()).to(eq([word1, word2]))
+    end
+  end
 end
